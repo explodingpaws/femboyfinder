2b9fe1fbd1c9c48cd27df62993c52f9ea2ec88dc
explosive pawbeans <36313403+explodingpaws@users.noreply.github.com>
2024-08-19 21:38:19 -0400

    update

--- discordbot.py
+++ discordbot.py
@@ -10,15 +10,19 @@ load_dotenv()
 api = E621()
 session = requests.session()
 intents = nextcord.Intents.default()
-bot = nextcord.BotIntegration
 
 botMain = nextcord.Client(intents=intents, activity=nextcord.Game(name='find femboys with /find'))
 
-defaultBlacklist = [
+token = os.getenv('botToken')
+tags = os.getenv('tagList')
+
+defaultTaglist = [
+    "rating:explicit",
+    "~femboy",
+    "~gay",
     "-shota",
     "-fnaf",
     "-loli",
-    "-sonic_(series)",
     "-vore",
     "-fart",
     "-scat",
@@ -27,29 +31,25 @@ defaultBlacklist = [
     "-watersports",
     "-poop",
     "-vomit",
-    "-obese",
+    "-morbidly_obese",
     "-fart",
     "-prolapse",
-    "-pokemon",
     "-cub",
     "-gore",
-    "-my_little_pony",
-    "-family_guy",
     "-incest",
     "-nazi",
     "-zoophilia"
     "-disembowelment",
-    "-child",
-    "-nintendo",
     "-young",
-    "-undertale",
-    "-deltarune"
+    "-feral",
+    "-undertale_(series)",
+    "-five_nights_at_freddy's",
+    "-nintendo",
+    "-pokemon",
+    "-my_little_pony",
+    "-sonic_the_hedgehog_(series)"
 ]
-defaultWhitelist = [
-    "rating:explicit",
-    "~femboy",
-    "~gay"
-]
+
 # all whitelist/blacklist items must be separated by commas and put into quotation marks
 # all filters and tags can be found at the cheatsheet link below
 # https://e621.net/help/cheatsheet
@@ -61,72 +61,75 @@ async def on_ready():
     print("Ready!")
 # logs in the bot
 
-@botMain.slash_command(name="find", description="find femboys", nsfw=True)
+@botMain.slash_command(name="finddebug", description="find femboys")
 async def find(interaction=nextcord.Interaction,
                videos_only: bool = nextcord.SlashOption(description="Require posts to be video", required=False),
                minimum_score: int = nextcord.SlashOption(description="Minimum score value", required=False),
                tags: str = nextcord.SlashOption(description="Custom tags (seperate with commas)", required=False)
                ):
     scoreRequirement = 40  # arbitrarily set this based on a lot of content being more eh as it goes down
-    if minimum_score is not None:
-        scoreRequirement = minimum_score
-    scoreString = "score:>=" + str(scoreRequirement)
-
-    tagList = [
-        scoreString,
-        "order:random"
-    ]
-
-    if tags is not None:
-        tagsAdded = [
-            tags
-        ]
-        tagList.extend(tagsAdded)
-    if videos_only is not None:
-        tagsAdded = [
-            "video"
-        ]
-        tagList.extend(tagsAdded)
-        # Video only filter (webm, mp4, mov)
-
-    tagList.extend(defaultBlacklist)
-    tagList.extend(defaultWhitelist)
-
-    postSearch = api.posts.search(tagList, ignore_pagination=True, limit=50)
-    # adds the tag lists and searches through api. USE PACKAGE 0.0.6 OF THE API.
-    # the other versions are broken for whatever reason.
-    # do not post issues regarding that please
-
-    maxPosts = len(postSearch)
-    if maxPosts == 0:
-        print("no posts found")
-        await interaction.response.send_message("no posts found")
+    if not interaction.channel.is_nsfw():
+        await interaction.response.send_message("not an nsfw channel")
     else:
-        randomPost = postSearch[random.randrange(0, maxPosts)]
-        if videos_only:
-            for count in range(25):
-                if randomPost.file.ext == "webm" or randomPost.file.ext == "mov" or randomPost.file.ext == "mp4":
-                    url = "https://e621.net/posts/" + str(randomPost.id)
-                    embedLink = "[Link To Video](" + url + ")"
-                    await interaction.response.send_message(embedLink)
-                    break
-                else:
-                    randomPost = postSearch[random.randrange(0, maxPosts)]
-                if count == 25:
-                    await interaction.response.send_message("no videos could be found")
-            # repeats loop a few times since sometimes people label other file formats as "video"
-            # gifs can be posted as regular embeds
+        if minimum_score is not None:
+            scoreRequirement = minimum_score
+        scoreString = "score:>=" + str(scoreRequirement)
+
+        tagList = [
+            scoreString,
+            "order:random"
+        ]
+        if tags is not None:
+            tagsAdded = [
+                tags
+            ]
+            tagList.extend(tagsAdded)
+        if videos_only is not None:
+            tagsAdded = [
+                "video"
+            ]
+            tagList.extend(tagsAdded)
+            # Video only filter (webm, mp4, mov)
+
+        tagList.extend(defaultTaglist)
+
+        postSearch = api.posts.search(tagList, ignore_pagination=True, limit=50)
+        # adds the tag lists and searches through api. USE PACKAGE 0.0.6 OF THE API.
+        # the other versions are broken for whatever reason.
+        # do not post issues regarding that please
+
+        maxPosts = len(postSearch)
+        if maxPosts == 0:
+            print("no posts found")
+            await interaction.response.send_message("no posts found")
         else:
-            url = "https://e621.net/posts/" + str(randomPost.id)
-            embedLink = "[Link To Image](" + url + ")"
-            embedObj = nextcord.Embed(title="Artist: " + randomPost.tags.artist[0],
-                                      description=embedLink + "\n Score of: " + str(randomPost.score.total),
-                                      color=0x00549E)
-            embedObj.set_image(url=randomPost.file.url)
-            embedObj.set_footer(text="Post from e621",
-                                icon_url="https://static.wikia.nocookie.net/logopedia/images/0/0b/Logo_transparent.svg/revision/latest/scale-to-width-down/300?cb=20181119223528g")
-            await interaction.response.send_message(embed=embedObj)
-            # includes a little footer image, artist (if valid) and score
+            randomPost = postSearch[random.randrange(0, maxPosts)]
+            if videos_only:
+                for count in range(25):
+                    if randomPost.file.ext == "webm" or randomPost.file.ext == "mov" or randomPost.file.ext == "mp4":
+                        url = "https://e621.net/posts/" + str(randomPost.id)
+                        embedLink = "[Link To Video](" + url + ")"
+                        await interaction.response.send_message(embedLink)
+                        break
+                    else:
+                        randomPost = postSearch[random.randrange(0, maxPosts)]
+                    if count == 25:
+                        await interaction.response.send_message("no videos could be found")
+                # repeats loop a few times since sometimes people label other file formats as "video"
+                # gifs can be posted as regular embeds
+            else:
+                url = "https://e621.net/posts/" + str(randomPost.id)
+                embedLink = "[Link To Image](" + url + ")"
+                embedObj = nextcord.Embed(title="Artist: " + randomPost.tags.artist[0],
+                                          description=embedLink + "\n Score of: " + str(randomPost.score.total),
+                                          color=0x00549E)
+                embedObj.set_image(url=randomPost.file.url)
+                embedObj.set_footer(text="Post from e621",
+                                    icon_url="https://static.wikia.nocookie.net/logopedia/images/0/0b/Logo_transparent.svg/revision/latest/scale-to-width-down/300?cb=20181119223528g")
+                await interaction.response.send_message(embed=embedObj)
+                # includes a little footer image, artist (if valid) and score
+                
+botMain.run(token)
 
 token = os.getenv('botToken')
 botMain.run(token)
